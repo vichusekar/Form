0114e219919625c646b96eef9181d39b53e5ae6e
Vignesh Rajasekar <127473360+vichusekar@users.noreply.github.com>
2023-04-30 09:37:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function foo(){
+    var fname=document.getElementById('main').value
+    console.log(`Firstname ${fname}`)
+
+    var lname=document.getElementById('main1').value
+    console.log(`Lastname ${lname}`)
+
+    var address=document.getElementById('main2').value
+    console.log(`Address ${address}`)
+
+    var pcode=document.getElementById('main3').value
+    console.log(`Pincode ${pcode}`)
+
+    var gender=document.getElementById('main4').value
+    console.log(`Gender ${gender}`)
+
+    var mail=document.getElementById('main8').value
+    console.log(`Email ${mail}`)
+
+    var state=document.getElementById('main6').value
+    console.log(`State ${state}`)
+
+    var country=document.getElementById('main7').value
+    console.log(`Country ${country}`)
+
+
+}
